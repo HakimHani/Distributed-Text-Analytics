7911852721c6d04745bb03a2026ebc20057f8010
Hakim Hani <hakeem@my.yorku.ca>
2019-12-03 10:52:08 -0500

    initial commit

---
+++ B/checkinsmapper.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+import csv
+import re
+
+infile = sys.stdin
+next(infile)    
+for line in infile:
+    # getting the 'business_id' column from the CSV
+    business_id = line.split(",")[0].strip()
+    
+    weekday = line.split(",")[1].strip()
+
+    checkins = line.split(",")[3].strip()
+
+    print("%s %s\t%s" % (business_id,weekday,checkins))
